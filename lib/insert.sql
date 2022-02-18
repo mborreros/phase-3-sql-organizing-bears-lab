aa517346614976e1e1b1e66e653f7c5dbeb50eaa
mborreros <mlbsindia@gmail.com>
2022-02-18 11:41:49 -0500

    commit tree 5794e692a90a53914856cd0df3d3c816165b4427

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "kind", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 54, "F", "white", "wild", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "light brown", "baby", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 37, "M", "dark brown", "aggressive", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "black", "quiet", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 41, "M", "slime green", "grouchy", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 21, "F", "grey", "caring", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "cream", NULL, 1);
